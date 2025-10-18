cf7c7a67f5954bc19acd3f6265442540d66779f9
est <baconcat1912@gmail.com>
2025-10-18 10:17:39 +0800

    Initial commit

---
+++ src/client/java/org/bacon/noviaversionkick/client/NoviaversionkickDataGenerator.java
@@ -0,0 +1,12 @@
+package org.bacon.noviaversionkick.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NoviaversionkickDataGenerator implements DataGeneratorEntrypoint {
+
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+    }
+}
---
+++ src/main/java/org/bacon/noviaversionkick/Noviaversionkick.java
@@ -0,0 +1,10 @@
+package org.bacon.noviaversionkick;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Noviaversionkick implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
